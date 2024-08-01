4368637221d37fe5a8cb046d7417aa6b1f20fcf8
anna-hub19 <annabastosbeatriz@gmail.com>
2024-08-01 09:25:49 -0300

    update css form

--- src/components/cadastrarVideo/formulario/formulario.js
+++ src/components/cadastrarVideo/formulario/formulario.js
@@ -3,30 +3,36 @@ import './formulario.css';
 function Formulario() {
     return (
         <>
-            <div>
+            <div className='div-titulo'>
                 <h3>
                     Criar Card
                 </h3>
             </div>
-            <form>
-                <label for='titulo'>titulo </label>
-                <input id='titulo' type='text' placeholder='ingrese el título' />
+            <form className='container-form'>
+                <div>
 
-                <label for="categoria">categoria</label>
-                <select id="categoria" name="categoria">
-                    <option value="selecione">selecione uma categoria</option>
-                    <option value="front-end">front-end</option>
-                    <option value="back-end">back-end</option>
-                    <option value="mobile">mobile</option>
-                </select>
-                <label for='imagem'>imagem </label>
-                <input id='imagem' type='text' placeholder='ingrese el imagem' />
+                    <label for='titulo'>titulo </label>
+                    <input className='input-form' id='titulo' type='text' placeholder='ingrese el título' />
 
-                <label for='video'>video </label>
-                <input id='video' type='text' placeholder='ingrese el video' />
+                    <label for="categoria">categoria</label>
+                    <select className='input-form'  id="categoria" name="categoria">
+                        <option value="selecione">selecione uma categoria</option>
+                        <option value="front-end">front-end</option>
+                        <option value="back-end">back-end</option>
+                        <option value="mobile">mobile</option>
+                    </select>
+                </div>
+                <div>
+                    <label for='imagem'>imagem </label>
+                    <input className='input-form' id='imagem' type='text' placeholder='ingrese el imagem' />
 
-                <label for='descricao'>descrição </label>
-                <input id='descricao' type='text' placeholder='sobre o que é esse vídeo?' />
+                    <label for='video'>video </label>
+                    <input className='input-form' id='video' type='text' placeholder='ingrese el video' />
+                </div>
+                <div>
+                    <label for='descricao'>descrição </label>
+                    <input className='input-form' id='descricao' type='text' placeholder='sobre o que é esse vídeo?' />
+                </div>
                 <button>GUARDAR</button>
                 <button>LIMPAR</button>
             </form>
